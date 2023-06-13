d6a8a306195c98f6dbfcdf6fbf3a1861f22836ad
Pablo Gallar <pablo.gallar@gmail.com>
2023-06-13 09:39:08 -0300

    Update global-second.py

--- global-second.py
+++ global-second.py
@@ -4,23 +4,21 @@ from ipytv import playlist
 from ipytv.playlist import M3UPlaylist
 from ipytv.channel import IPTVChannel
 
-PORT = 80
+PORT = 8000
 
 class RequestHandler(http.server.BaseHTTPRequestHandler):
     def do_GET(self):
         url = "http://tv.dominiotv.xyz:25461/get.php?username=Rolando&password=Rolando2021&type=m3u_plus"
         pl = playlist.loadu(url)
 
-        plGlobal = M3UPlaylist()
-
         for channel in pl:
-            channel.attributes["tvg-id"] = channel.attributes["tvg-name"]
-            plGlobal.append_channel(channel)
+            if (channel.attributes["tvg-id"] == ""):
+                channel.attributes["tvg-id"] = channel.attributes["tvg-name"]
 
         self.send_response(200)
         self.send_header("Content-Type", "text/txt")
         self.end_headers()
-        self.wfile.write(plGlobal.to_m3u_plus_playlist().encode())
+        self.wfile.write(pl.to_m3u_plus_playlist().encode())
 
 if __name__ == '__main__':
     serverAddress = ('', PORT)
